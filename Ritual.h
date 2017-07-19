2d17f9226a43d9c52bd883d057b26f6d6b079351
Jenn <shiyating1998@gmail.com>
2017-07-19 15:38:54 -0700

    Update Ritual.h

--- Ritual.h
+++ Ritual.h
@@ -1,12 +1,15 @@
 #include <vector>
 #include <string>
-
+#include <Card.h>
 class Ritual: public Card { //abstract
 
 	Ability *ability;
+	int activationCost;
 	int charges;
 
 public:
+    Boolean canActivate();
+    virtual void useAbility(Target *t=nullptr)=0;
 	virtual ~Ritual();
 
 };
