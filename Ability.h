e222b062da3487db9e1f33e10a8b6dccdf67502c
Jenn <shiyating1998@gmail.com>
2017-07-19 15:37:20 -0700

    gives a default parameter for activate

--- Ability.h
+++ Ability.h
@@ -1,17 +1,17 @@
 #include <vector>
 #include <string>
 
-class Ability { 
+class Ability {
 
 	string description; //attack points
 	int cost; //defense points
 	// type of trigger
-	enum SubscriptionType { startTurn, endTurn, minionBirth, minionDeath, onPlay}; 
+	enum SubscriptionType { startTurn, endTurn, minionBirth, minionDeath, onPlay};
 
 public:
 	// activate ability, on optional target
-	bool Activate(Target *target); 
-	
+	bool Activate(Target *target=nullptr);
+
 	virtual ~Ability();
 
 };
